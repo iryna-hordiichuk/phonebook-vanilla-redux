8dfadb6075082db92285cc86efdfe884e15b5446
Iryna Hordiichuk <72663259+IrynaHordiichuk@users.noreply.github.com>
2023-03-10 20:22:53 +0000

    adds lifecycle methods, contacts are saved in localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,6 +21,22 @@ export class App extends Component {
     filter: '',
   };
 
+componentDidMount() { 
+const savedContacts = localStorage.getItem('contacts');
+
+if(savedContacts !== null){
+  this.setState({contacts: JSON.parse(savedContacts)})
+}
+ }
+
+componentDidUpdate(_, prevState) {
+
+  if(prevState.contacts !== this.state.contacts){
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  };
+
+};
+
   deleteContact = id => {
     this.setState(prevState => ( {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
