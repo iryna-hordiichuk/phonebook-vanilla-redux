1b147c4fd382d02c3f233751e4794059f0788f5e
Iryna Hordiichuk <72663259+IrynaHordiichuk@users.noreply.github.com>
2023-12-23 18:14:32 +0000

    removed unused import

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,7 +3,7 @@ import { deleteContact } from "../../redux/actions";
 import PropTypes from 'prop-types';
 import { Item } from './ContactItem.styled';
 import { Button } from 'components/ContactForm/ContactForm.styled';
-import { createGlobalStyle } from "styled-components";
+
 
 export const ContactItem = ({ id, name, number}) => {
   const dispatch = useDispatch();
